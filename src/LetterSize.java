bc1bff4041e98d8b4336f61ad5bae3450ea48fd2
samiaasad <samiaasad604@gmail.com>
2024-03-15 17:48:17 +0600

    E4.2

--- src/LetterSize.java
+++ src/LetterSize.java
@@ -1,12 +1,14 @@
+import java.util.*;
 public class LetterSize{
     public static void main(String[] args){
-        double width = 8.5 * 25.4;
-        double height = 11 * 25.4;
-        double perimeter = 2 * (width + height);
-        double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-        System.out.printf("Width: %8.2f mm\n", width);
-        System.out.printf("Height: %8.2f mm\n", height);
-        System.out.printf("Perimeter: %8.2f mm\n", perimeter);
-        System.out.printf("Diagonal: %.2f mm\n", diagonal);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a value: ");
+        int i = sc.nextInt();
+        double square = (i * i);
+        double cube = (i * i * i);
+        double fourth = (Math.pow(i, 4));
+        System.out.printf("Square: %8.2f \n", square);
+        System.out.printf("Cube: %8.2f \n", cube);
+        System.out.printf("Fourth: %8.2f \n", fourth);
     }
 }
